f9f244172e235a0c9e1221598445cd4178b6a397
zaidohDev <danfealmeida@gmail.com>
2019-11-30 17:17:30 -0300

    classe funcionario

---
+++ apps/funcionarios/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Funcionario
+# Register your models here.
+admin.site.register(Funcionario)
--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 # Create your models here.
-class Empresa(models.Model):
-    nome = models.CharField(max_length=100, help_text="Nome da Empresa")
+class Funcionario(models.Model):
+    nome = models.CharField(max_length=100, help_text="Nome")
 
     def __str__(self):
         return self.nome
