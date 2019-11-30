fe0be1bfc6fa278288462cb62528a854007fd52f
zaidohDev <danfealmeida@gmail.com>
2019-11-30 17:43:38 -0300

    classe departamento srt

--- apps/departamentos/models.py
+++ apps/departamentos/models.py
@@ -4,3 +4,6 @@ from django.db import models
 # Create your models here.
 class Departamento(models.Model):
     nome = models.CharField(max_length=100, help_text="Departamentos")
+
+    def __str__(self):
+        return self.nome
